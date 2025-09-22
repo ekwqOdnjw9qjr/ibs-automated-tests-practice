df8608291f2b5aa43a4079b200913ae6b622e26b
Mironov_Roman <psychokid534@gmail.com>
2025-09-22 21:16:47 +0300

    Update hooks.

--- src/test/java/com/ibs/cucumber/hooks/Hooks.java
+++ src/test/java/com/ibs/cucumber/hooks/Hooks.java
@@ -1,29 +1,18 @@
 package com.ibs.cucumber.hooks;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 
 public class Hooks {
 
-    private WebDriver driver;
-
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        WebDriverRunner.setWebDriver(driver);
+        Selenide.open();
+        WebDriverRunner.getWebDriver().manage().window().maximize();
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
                 .savePageSource(false));
@@ -31,9 +20,6 @@ public class Hooks {
 
     @After
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-            WebDriverRunner.closeWebDriver();
-        }
+        Selenide.closeWebDriver();
     }
 }
